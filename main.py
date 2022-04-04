1a3da715d715efb7cb0c984eeaa9b547e387c183
Jitske Arends <80577121+happygeekme@users.noreply.github.com>
2022-04-04 16:45:11 +0200

    nog een testje

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello, world! Hello, world! Het is me gelukt!'
+    return 'Hello, world! Hello, world! Het is me gelukt! Nelius en Samuel zijn er ook bij!'
