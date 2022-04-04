5f6ed815a970054014f12f9384251df281e90a35
Jitske Arends <80577121+happygeekme@users.noreply.github.com>
2022-04-04 16:41:41 +0200

    final test

--- test_main.py
+++ test_main.py
@@ -3,5 +3,5 @@ from main import app
 def test_index():
     response = app.test_client().get('/')
     
-    assert response.data == b'Hello, world!'
+    assert response.data == b'Hello, world! Hello, world! Het is me gelukt!'
     assert response.status_code == 200
