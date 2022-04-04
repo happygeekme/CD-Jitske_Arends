05d17f17161566d4b85dcdebd93ea10ae7f3443c
Jitske Arends <80577121+happygeekme@users.noreply.github.com>
2022-04-04 13:53:05 +0200

    first commit clean slate

---
+++ test_main.py
@@ -0,0 +1,7 @@
+from main import app
+
+def test_index():
+    response = app.test_client().get('/')
+    
+    assert response.data == b'Hello, world! Hello, world! Hello, world!'
+    assert response.status_code == 200
