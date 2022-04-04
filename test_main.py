9b2eecdf03865ec04d818df75102a66acda8c46f
Jitske Arends <80577121+happygeekme@users.noreply.github.com>
2022-04-04 13:57:09 +0200

    correct test

--- test_main.py
+++ test_main.py
@@ -3,5 +3,5 @@ from main import app
 def test_index():
     response = app.test_client().get('/')
     
-    assert response.data == b'Hello, world! Hello, world! Hello, world!'
+    assert response.data == b'Hello, world!'
     assert response.status_code == 200
